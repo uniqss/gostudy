74b2b48511b57f91eb5118f74f35357365086b00
uniqs <uniqs@163.com>
2020-08-20 13:55:26 +0800

    export a zerolog.Logger instead

--- log_test/log_test_zerolog/log_wrapper/log_wrapper.go
+++ log_test/log_test_zerolog/log_wrapper/log_wrapper.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-var LogLevelWriter zerolog.LevelWriter
+var ULogger zerolog.Logger
 
 type FilteredWriter struct {
 	w     zerolog.LevelWriter
@@ -23,8 +23,17 @@ func (w *FilteredWriter) WriteLevel(level zerolog.Level, p []byte) (n int, err e
 	return len(p), nil
 }
 
-func InitLog(logFileName string, logFolder string) error {
-	logFileFlag := os.O_APPEND|os.O_CREATE|os.O_WRONLY
+func openLogFile(fileName string, flag int, fileMode os.FileMode) (*os.File, error) {
+	fDebug, err := os.OpenFile(fileName, flag, fileMode)
+	if err != nil {
+		fmt.Println("openLogFile failed. fileName:", fileName, " err:", err)
+		return nil, err
+	}
+	return fDebug, nil
+}
+
+func InitLog(logFileName string, logFolder string, allLog bool) error {
+	logFileFlag := os.O_APPEND | os.O_CREATE | os.O_WRONLY
 	var logFilePerm os.FileMode = 0644
 	if logFolder == "" {
 		logFolder = "./"
@@ -35,12 +44,27 @@ func InitLog(logFileName string, logFolder string) error {
 			return err
 		}
 	}
-	fAll, _ := os.OpenFile(logFolder +logFileName+ "_all.log", logFileFlag, logFilePerm)
-	fDebug, _ := os.OpenFile(logFolder +logFileName+ "_debug.log", logFileFlag, logFilePerm)
-	fInfo, _ := os.OpenFile(logFolder +logFileName+ "_info.log", logFileFlag, logFilePerm)
-	fWarn, _ := os.OpenFile(logFolder +logFileName+ "_warn.log", logFileFlag, logFilePerm)
-	fError, _ := os.OpenFile(logFolder +logFileName+ "_error.log", logFileFlag, logFilePerm)
-	fFatal, _ := os.OpenFile(logFolder +logFileName+ "_fatal.log", logFileFlag, logFilePerm)
+
+	fDebug, err := openLogFile(logFolder+logFileName+"_debug.log", logFileFlag, logFilePerm)
+	if err != nil {
+		return err
+	}
+	fInfo, err := openLogFile(logFolder+logFileName+"_info.log", logFileFlag, logFilePerm)
+	if err != nil {
+		return err
+	}
+	fWarn, err := openLogFile(logFolder+logFileName+"_warn.log", logFileFlag, logFilePerm)
+	if err != nil {
+		return err
+	}
+	fError, err := openLogFile(logFolder+logFileName+"_error.log", logFileFlag, logFilePerm)
+	if err != nil {
+		return err
+	}
+	fFatal, err := openLogFile(logFolder+logFileName+"_fatal.log", logFileFlag, logFilePerm)
+	if err != nil {
+		return err
+	}
 
 	writerDebug := zerolog.MultiLevelWriter(fDebug)
 	writerInfo := zerolog.MultiLevelWriter(fInfo)
@@ -54,13 +78,28 @@ func InitLog(logFileName string, logFolder string) error {
 	filteredWriterError := &FilteredWriter{writerError, zerolog.ErrorLevel}
 	filteredWriterFatal := &FilteredWriter{writerFatal, zerolog.FatalLevel}
 
-	LogLevelWriter = zerolog.MultiLevelWriter(fAll, zerolog.ConsoleWriter{Out: os.Stdout},
-		filteredWriterDebug, filteredWriteInfo, filteredWriterWarn, filteredWriterError, filteredWriterFatal)
+	if allLog {
+		fAll, err := openLogFile(logFolder+logFileName+"_all.log", logFileFlag, logFilePerm)
+		if err != nil {
+			return err
+		}
+		logLevelWriter := zerolog.MultiLevelWriter(
+			fAll,
+			zerolog.ConsoleWriter{Out: os.Stdout},
+			filteredWriterDebug, filteredWriteInfo, filteredWriterWarn, filteredWriterError, filteredWriterFatal)
+
+		ULogger = zerolog.New(logLevelWriter)
+	} else {
+		logLevelWriter := zerolog.MultiLevelWriter(
+			zerolog.ConsoleWriter{Out: os.Stdout},
+			filteredWriterDebug, filteredWriteInfo, filteredWriterWarn, filteredWriterError, filteredWriterFatal)
+
+		ULogger = zerolog.New(logLevelWriter)
+	}
 
 	// usage
-	//uLog := zerolog.New(log_wrapper.LogLevelWriter).With().Str("func", "main").Timestamp().Logger()
+	//uLog := zerolog.New(log_wrapper.logLevelWriter).With().Str("func", "main").Timestamp().Logger()
 	//uLog.Debug().Str("hello", "world")
 
 	return nil
 }
-
--- log_test/log_test_zerolog/main2.go
+++ log_test/log_test_zerolog/main2.go
@@ -2,19 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/rs/zerolog"
 	"log_test_zerolog/log_wrapper"
 	"strings"
 )
 
-func main() {
-	err := log_wrapper.InitLog("game_server", "./log/")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	uLog := zerolog.New(log_wrapper.LogLevelWriter).With().Timestamp().Str("func", "main").Logger()
+func testLog(){
+	uLog := log_wrapper.ULogger.With().Timestamp().Str("func", "testLog").Logger()
 
 	uLog.Debug().Str("hello", "world").Msg(" Some Debug log 我是中文哈哈")
 	uLog.Info().Strs("xixi", []string{"haha", "hehe"}).Msg(" Some  Info log 呵呵吼吼")
@@ -23,6 +16,18 @@ func main() {
 	// !!! fatal log will exit the program
 	//uLog.MainLog.Fatal().Msg(" Some Fatal log! 不的了了。。。机房冒烟了")
 
+}
+
+func main() {
+	err := log_wrapper.InitLog("gameserver", "./log/", true)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	testLog()
+
+
 	var input string
 	for {
 		fmt.Scanln(&input)
@@ -30,6 +35,7 @@ func main() {
 		if input == "e" || input == "exit" {
 			break
 		} else {
+			uLog := log_wrapper.ULogger.With().Timestamp().Str("func", "main").Logger()
 			uLog.Info().Str("input", input).Msg("main. user input")
 		}
 	}
