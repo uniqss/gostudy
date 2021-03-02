04375f070c1edc964979c433ecee842b5075c160
uniqss <uniqs@163.com>
2021-03-02 11:49:35 +0800

    www

--- study/http_curr_dir.go
+++ study/http_curr_dir.go
@@ -7,16 +7,23 @@ import (
 	"net/http"
 )
 
+/*
+start_share.bat:
+start http_curr_dir.exe -dir ./share
+*/
+
 func main() {
 	var host string
 	var port int
+	var dir string
 
 	flag.StringVar(&host, "h", "0.0.0.0", "listen host. default 0.0.0.0")
-	flag.IntVar(&port, "p", 8080, "listen port. default 8080")
+	flag.IntVar(&port, "p", 60000, "listen port. default 8080")
+	flag.StringVar(&dir, "dir", "./", "local dir to share")
 
 	flag.Parse()
 
-	h := http.FileServer(http.Dir("./"))
+	h := http.FileServer(http.Dir(dir))
 	//err := http.ListenAndServe(":8080", h)
 	fmt.Println("host:", host, " port:", port)
 	err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), h)
